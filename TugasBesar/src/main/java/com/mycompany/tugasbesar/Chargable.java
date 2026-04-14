722110aaafb35b203b67f9a84cfbb9ce019a16f4
kiii <muhammadrifkyalfajri@gmail.com>
2026-04-14 22:41:05 +0700

    Add files via upload

---
+++ TugasBesar/src/main/java/com/mycompany/tugasbesar/Chargable.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.tugasbesar;
+
+/**
+ *
+ * @author rifky
+ */
+public interface Chargable {
+    public void recharge();
+    public double checkBatteryStatus();
+}
---
+++ TugasBesar/src/main/java/com/mycompany/tugasbesar/TugasBesar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tugasbesar;
+
+/**
+ *
+ * @author rifky
+ */
+public class TugasBesar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
